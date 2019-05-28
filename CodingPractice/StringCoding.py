7a6adf8a196ce7a33b86afecc4dff1797f17ef2b
kiran kumar <kirankumarhs@integramicro.co.in>
2019-05-29 00:01:54 +0530

    Reverse string program

--- CodingPractice/StringCoding.py
+++ CodingPractice/StringCoding.py
@@ -1,42 +1,10 @@
-# Python program to reverse a string without stack 
-# using extended slice syntax 
+# Python program to reverse a string using slice method
 
-# 1 Function to reverse a string
-def fisrtReverseMethod(string):
+def reverse(string):
     string = string[::-1]
     return string
 
 
-# 2 Python code to reverse a string  
-# using loop 
-
-def secondReverseMethod(string):
-    str = ""
-    for i in string:
-        str = i + str
-    return str
-
-
-# 3 Python code to reverse a string  
-# using recursion 
-
-def thirdReverseMethod(string):
-    if len(string) == 0:
-        return string
-    else:
-        return thirdReverseMethod(string[1:]) + string[0]
-
-    # 4 Python code to reverse a string
-
-
-# using reversed()
-
-# Function to reverse a string 
-def forthReverseString(string):
-    string = "".join(reversed(string))
-    return string
-
-
-str = input("Plase enter the string    ")
-rev_str = forthReverseString(str)
-print("Original String is : ", str, "   And Reverse String is : ", rev_str)
+inString = input("Please enter the string    ")
+rev_str = reverse(inString)
+print("Reverse String is : ", rev_str)
